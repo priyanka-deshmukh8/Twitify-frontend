ed6d3c809f027003afbb9a06bcb225b98690c35d
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2023-05-10 01:03:01 +0530

    footer changed

--- src/components/aside/aside-footer.tsx
+++ src/components/aside/aside-footer.tsx
@@ -28,7 +28,7 @@ export function AsideFooter(): JSX.Element {
           </a>
         ))}
       </nav>
-      <p>© 2023 Twitter, Inc.</p>
+      <p>© 2023 Twitify, Inc.</p>
     </footer>
   );
 }
