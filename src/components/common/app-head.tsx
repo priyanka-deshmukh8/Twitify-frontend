bd47dee62af96d1fa4c09f65911c2688aa352c7f
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2023-05-10 00:58:13 +0530

    twitter changed to twitify

--- src/components/common/app-head.tsx
+++ src/components/common/app-head.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export function AppHead(): JSX.Element {
   return (
     <Head>
-      <title>Twitter</title>
+      <title>Twitify</title>
       <meta name='og:title' content='Twitter' />
       <link rel='icon' href='/favicon.ico' />
       <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
